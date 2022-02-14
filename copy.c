117c1f1e8eebbeb9e4b64a3bedce70ebe8c046e5
Sober7135 <soberwu@outlook.com>
2022-02-14 23:12:50 +0800

    del duplicate.c and creat copy.c. use cl to copy

---
+++ copy.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+  if (argc == 1) {
+    fprintf(stderr, "missing file operand\n");
+    exit(EXIT_FAILURE);
+  } else if (argc == 2) {
+    fprintf(stderr, "missing destination file operand after '%s'", argv[1]);
+    exit(EXIT_FAILURE);
+  } else if (argc > 3) {
+    exit(EXIT_FAILURE);
+  }
+
+  FILE* fp_r;
+  FILE* fp_w;
+  int rgb;
+  fp_r = fopen(argv[1], "rb");
+  fp_w = fopen(argv[2], "wb");
+  if (!fp_r) {
+    fprintf(stderr, "error in opening the target file\n");
+    exit(EXIT_FAILURE);
+  }
+  if (!fp_w) {
+    fprintf(stderr, "error in creating the new file \n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (fread(&rgb, 1, 1, fp_r)) {
+    fwrite(&rgb, 1, 1, fp_w);
+    rgb &= 0;
+  }
+  fclose(fp_r);
+  fclose(fp_w);
+  return 0;
+}
